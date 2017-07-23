347a18256d6541ed60d012a763ed374ba51c05e8
robinarthur <stukka@gmx.de>
2017-07-23 23:50:38 +0200

    README.md

--- quickGrab.py
+++ quickGrab.py
@@ -2,8 +2,12 @@ from PIL import ImageGrab
 import os
 import time
 
+'''
+Es wird als erstes ein Screenshot gemacht
+'''
+
 def screenGrab():
-    box = ()
+    box = (100,50,400,400)
     im = ImageGrab.grab()
     im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +
 '.png', 'PNG')
