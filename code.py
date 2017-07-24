1a09221fb40e397e791b3c9461bdee440b6197e7
robinarthur <stukka@gmx.de>
2017-07-24 16:49:17 +0200

    Mouse Movement

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+#! C:\Users\tank\Anaconda3\python.exe
+
 from PIL import ImageGrab
 import os
 import time
@@ -30,23 +32,31 @@ def screenGrab():
 
 def leftClick():
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    time.sleep(.l)
+    time.sleep(.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
     print( "Click")    #completly optional. But nice for debugging purposes.
 
 def leftDown():
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    time.sleep(.l)
+    time.sleep(.1)
     print('left Down')
 
 def leftUp():
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-    time.sleep(.l)
+    time.sleep(.1)
     print('left release')
 
-def main():
-    screenGrab()
+def mousePos(cord):
+    win32api.SetCursorPos((x_pad + cord[0], y_pad + cord[1]))
 
+def get_cords():
+    x,y = win32api.GetCursorPos()
+    x = x - x_pad
+    y = y - y_pad
+    print(x,y)
+
+def main():
+    pass
 
 if __name__ == '__main__':
     main()
