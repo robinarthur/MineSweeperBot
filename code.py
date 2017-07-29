fe5732780a318bf5deaa958dc95dfefe9dcfd156
robinarthur <stukka@gmx.de>
2017-07-29 10:53:11 +0200

    Houghlines test

--- code.py
+++ code.py
@@ -5,6 +5,7 @@ import os
 import time
 import win32api, win32con
 import numpy as np
+import cv2
 
 """
 All coordinates assume a screen resolution of 1366x768, and the App is the left
@@ -49,6 +50,25 @@ def Grab():
     a = a.sum()
     print(a)
     return a
+
+img = cv2.imread(screenGrab())
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,150,apertureSize = 3)
+
+lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+for rho, theta in lines[0]:
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
+
+    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+cv2.imwrite('houghlines3.jpg', img)
 
 
 '''
