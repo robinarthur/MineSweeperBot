1c9d177514c0c9c1f64a0e0f1c36e79636e2b5fa
robinarthur <stukka@gmx.de>
2017-08-16 18:58:46 +0200

    rmv gradient()
    its actual all in canny_edge()

--- data/code.py
+++ data/code.py
@@ -311,33 +311,6 @@ def getRGB(img):
 
 
 
-def gradient():
-        # taken from:
-        # https://pythonprogramming.net/canny-edge-detection-gradients-python-opencv-tutorial/
-        for fn in glob('full_snap.png'):
-            img = cv2.imread(fn)
-            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-            lower_red = np.array([30,150,50])
-            upper_red = np.array([255,255,180])
-
-            mask = cv2.inRange(hsv, lower_red, upper_red)
-            res = cv2.bitwise_and(img, img, mask= mask)
-
-            laplacian = cv2.Laplacian(img, cv2.CV_64F)
-            sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
-            sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=5)
-
-            #cv2.imshow('Original', img)
-            #cv2.imshow('Mask', mask)
-            cv2.imshow('laplacian', laplacian)
-            #cv2.imshow('sobelx', sobelx)
-            #cv2.imshow('sobely', sobely)
-
-            cv2.waitKey()
-
-        cv2.destroyAllWindows
-
 def canny_edge():
         # taken from:
         # https://pythonprogramming.net/canny-edge-detection-gradients-python-opencv-tutorial/
