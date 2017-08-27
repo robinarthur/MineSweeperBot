f380b699c5eebf6a9a628b020b86f26cf62b519e
robinarthur <stukka@gmx.de>
2017-08-27 09:17:01 +0200

    next steps
    only 63/81 squares were filled, maybe i have to change to grayscale

--- data/stackoverflow.py
+++ data/stackoverflow.py
@@ -14,6 +14,8 @@ The blue tile coordinates are stored in coords, np_arr is my array.
 """
 import numpy as np
 import cv2
+import imutils
+import pandas as pd
 
 initial = np.arange(81).reshape(9, 9)
 print("initial_array"'{0}'.format(initial))
@@ -35,15 +37,50 @@ res = cv2.bitwise_and(img,img, mask= mask)
 cv2.imshow('img',img)
 cv2.imshow('mask',mask)
 cv2.imshow('res',res)
-
-im2, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-contour_img = cv2.drawContours(img, contours, -1, (0,255,0), 1)
-
-
-cv2.imshow('contour_img', contour_img)
 # This displays the frame, mask
 # and res which we created in 3 separate windows.
-cv2.waitKey(0)
+
+im2, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+contour_img = cv2.drawContours(img, contours, -1, (255,255,255), -1)
+cv2.imshow('contour_img', contour_img)
+
+# find contours in the thresholded image
+cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+"""
+print("cnts'{0}'".format(cnts))
+i = 0
+# loop over the contours
+for c in cnts:
+    # compute the center of the contour
+    M = cv2.moments(c)
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    i=+1
+    # draw the contour and center of the shape on the image
+    cv2.drawContours(contour_img, [c], -1, (0, 255, 0), 2)
+    cv2.circle(contour_img, (cX, cY), 7, (255, 255, 255), -1)
+    print("Durchgang: %5d ,X: %6d ,Y: %6d" (i,cX,cY)
+
+	cv2.imshow("Image", contour_img)
+    cv2.waitKey(0)
+"""
+# loop over the contours
+for c in cnts:
+	# compute the center of the contour
+	M = cv2.moments(c)
+	cX = int(M["m10"] / M["m00"])
+	cY = int(M["m01"] / M["m00"])
+
+	# draw the contour and center of the shape on the image
+	cv2.drawContours(contour_img, [c], -1, (0, 255, 0), 2)
+	cv2.circle(contour_img, (cX, cY), 7, (255, 255, 255), -1)
+
+	# show the image
+	cv2.imshow("Image", contour_img)
+	cv2.waitKey(0)
 
 
 # Destroys all of the HighGUI windows.
