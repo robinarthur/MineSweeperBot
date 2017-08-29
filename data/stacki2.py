0e491f70352c5e6e736453a76b6bb2e928ea47fe
robinarthur <stukka@gmx.de>
2017-08-29 07:12:07 +0200

    next try, i get so tired of opencv...

--- data/stacki2.py
+++ data/stacki2.py
@@ -3,13 +3,26 @@ import numpy as np
 
 img = cv2.imread('full_snap.png')
 
-blue_MIN = np.array([95, 50, 83],np.uint8)
-blue_MAX = np.array([105, 61, 100],np.uint8)
+blue_MIN = np.array([80, 50, 83])
+blue_MAX = np.array([105, 61, 100])
+"""
+https://pythonprogramming.net/color-filter-python-opencv-tutorial/
+"""
+lower_red = np.array([30,150,50])
+lower_red = cv2.cvtColor(lower_red,cv2.COLOR_BGR2HSV)
+upper_red = np.array([255,255,180])
+upper_red = cv2.cvtColor(upper_red,cv2.COLOR_BGR2HSV)
 
 hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
 frame_threshed = cv2.inRange(hsv_img, blue_MIN, blue_MAX)
-cv2.imshow('output2.jpg', frame_threshed)
+frame_threshed2 = cv2.inRange(frame_threshed, lower_red, upper_red)
+#out = cv2.cvtColor(frame_threshed,cv2.COLOR_HSV2BGR)
+cv2.imshow("input", img)
+cv2.imshow("hsv_img", hsv_img)
+cv2.imshow("frame_treshed", frame_threshed)
+cv2.imshow("frame_threshed2", frame_threshed2)
+#cv2.imshow('out',out)
 
 
 cv2.waitKey(0)
