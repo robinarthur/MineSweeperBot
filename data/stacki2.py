1b552213bef0fc6dafd45a90bfcd8e0230224e55
robinarthur <stukka@gmx.de>
2017-08-30 20:44:23 +0200

    one step closer
    # fill the dataframe with the coords

--- data/stacki2.py
+++ data/stacki2.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import imutils
+import pandas as pd
 
 img = cv2.imread('full_snap.png')
 """
@@ -33,7 +34,7 @@ frame_threshed = cv2.inRange(img, blue_MIN, blue_MAX)
 #out = cv2.cvtColor(frame_threshed,cv2.COLOR_HSV2BGR)
 cv2.imshow("input", img)
 #cv2.imshow("hsv_img", hsv_img)
-cv2.imshow("frame_treshed", frame_threshed)
+#cv2.imshow("frame_treshed", frame_threshed)
 #cv2.imshow("frame_threshed2", frame_threshed2)
 #cv2.imshow('out',out)
 
@@ -42,21 +43,36 @@ cnts = cv2.findContours(frame_threshed.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-print("cnts", cnts)
+#print("cnts", cnts)
+i = len(cnts)
+print("i", i)
+
+# we know we're gonna have x rows of data, where x is the product of
+# board_width * board_height
+numberOfRows = 81
+# create dataframe
+df = pd.DataFrame(index=np.arange(numberOfRows, 0), columns=('x', 'y'))
+
+print("df", df)
+
 
 for c in cnts:
 	# compute the center of the contour
 	M = cv2.moments(c)
 	cX = int(M["m10"] / M["m00"])
 	cY = int(M["m01"] / M["m00"])
-	i=+1
+	# fill the dataframe with the coords
+
+	i-=1
 	# draw the contour and the center of the shape on the image
-	cv2.drawContours(img, [c], -1, (255, 255, 2558), 2)
-	cv2.circle(img, (cX, cY), 7, (255, 255, 255), -1)
+	#cv2.drawContours(img, [c], -1, (255, 255, 255), 2)
+	cv2.circle(img, (cX, cY), 1, (255, 255, 255), -1)
 	#print("Durchgang: %5d ,X: %6d ,Y: %6d" (str(i),str(cX),str(cY)))
 
-	cv2.imshow("Image_with_contours",img)
-	cv2.waitKey(0)
+cv2.imshow("Image_with_contours",img)
+print("cX", cX)
+print("cY", cY)
+print("i_end", i)
 
 
 cv2.waitKey(0)
