0bd93336749c82fbcbe0636e00de7244098c6d7e
robinarthur <stukka@gmx.de>
2017-08-30 07:21:15 +0200

    upd
    image_with_contours had all the centers painted with a circle

--- data/stacki2.py
+++ data/stacki2.py
@@ -1,28 +1,62 @@
 import cv2
 import numpy as np
+import imutils
 
 img = cv2.imread('full_snap.png')
+"""
+R: between 98 and 128
+G: between 176 and 211
+B: between 255
 
-blue_MIN = np.array([80, 50, 83])
-blue_MAX = np.array([105, 61, 100])
+h: between 210 and 200
+s: between 100 and 48
+v: between 68 and 100
+
+hsv/2 in opencv and opencv uses BGR not RGB
+"""
+
+blue_MIN = np.array([255, 176, 98])
+blue_MAX = np.array([255, 211, 128])
 """
 https://pythonprogramming.net/color-filter-python-opencv-tutorial/
-"""
+
 lower_red = np.array([30,150,50])
 lower_red = cv2.cvtColor(lower_red,cv2.COLOR_BGR2HSV)
 upper_red = np.array([255,255,180])
 upper_red = cv2.cvtColor(upper_red,cv2.COLOR_BGR2HSV)
+"""
 
-hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+#hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
-frame_threshed = cv2.inRange(hsv_img, blue_MIN, blue_MAX)
-frame_threshed2 = cv2.inRange(frame_threshed, lower_red, upper_red)
+frame_threshed = cv2.inRange(img, blue_MIN, blue_MAX)
+#frame_threshed2 = cv2.inRange(frame_threshed, lower_red, upper_red)
 #out = cv2.cvtColor(frame_threshed,cv2.COLOR_HSV2BGR)
 cv2.imshow("input", img)
-cv2.imshow("hsv_img", hsv_img)
+#cv2.imshow("hsv_img", hsv_img)
 cv2.imshow("frame_treshed", frame_threshed)
-cv2.imshow("frame_threshed2", frame_threshed2)
+#cv2.imshow("frame_threshed2", frame_threshed2)
 #cv2.imshow('out',out)
+
+# find contours in the thresholded image
+cnts = cv2.findContours(frame_threshed.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+print("cnts", cnts)
+
+for c in cnts:
+	# compute the center of the contour
+	M = cv2.moments(c)
+	cX = int(M["m10"] / M["m00"])
+	cY = int(M["m01"] / M["m00"])
+	i=+1
+	# draw the contour and the center of the shape on the image
+	cv2.drawContours(img, [c], -1, (255, 255, 2558), 2)
+	cv2.circle(img, (cX, cY), 7, (255, 255, 255), -1)
+	#print("Durchgang: %5d ,X: %6d ,Y: %6d" (str(i),str(cX),str(cY)))
+
+	cv2.imshow("Image_with_contours",img)
+	cv2.waitKey(0)
 
 
 cv2.waitKey(0)
