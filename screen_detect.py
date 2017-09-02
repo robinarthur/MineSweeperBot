afd4d95864c717f13ceea8587ca3881e8eb19d05
robinarthur <stukka@gmx.de>
2017-09-02 18:17:17 +0200

    change from mss to autopy

--- screen_detect.py
+++ screen_detect.py
@@ -1,16 +1,3 @@
-from mss.windows import MSS
-from PIL import Image
-import cv2
-import numpy as np
+import autopy
 
-mon = {'top': 160, 'left': 160, 'width': 200, 'height': 200}
-
-sct = MSS()
-
-while 1:
-    sct.get_pixels(mon)
-    img = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
-    cv2.imshow('test', np.array(img))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+img = autopy.bitmap.capture_screen().save('screengrab.png')
