f4570f38a037323b3e08a53c58156d5327a05302
robinarthur <stukka@gmx.de>
2017-09-02 20:17:35 +0200

    upd

--- screen_detect.py
+++ screen_detect.py
@@ -1,3 +1,10 @@
 import autopy
 
-img = autopy.bitmap.capture_screen().save('screengrab.png')
+
+ay = autopy
+width, height = ay.screen.get_size()
+img = ay.bitmap.capture_screen().save('screengrab.png')
+
+print(width,height)
+print(width)
+print(height)
