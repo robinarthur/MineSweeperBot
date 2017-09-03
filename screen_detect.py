c39fc78078ec219540cf724abed46204a36210a1
robinarthur <stukka@gmx.de>
2017-09-03 21:30:22 +0200

    have to test it
    maybe tomorrow

--- screen_detect.py
+++ screen_detect.py
@@ -14,3 +14,26 @@ blue_MAX = np.array([255, 211, 128])
 
 for w_pixel in width:
     for h_pixel in height:
+        pass
+
+"""
+https://stackoverflow.com/questions/3586046/fastest-way-to-take-a-screenshot-with-python-on-windows?rq=1
+
+"""
+import win32gui
+import win32ui
+
+hwnd = win32gui.FindWindow(None, windowname)
+wDC = win32gui.GetWindowDC(hwnd)
+dcObj=win32ui.CreateDCFromHandle(wDC)
+cDC=dcObj.CreateCompatibleDC()
+dataBitMap = win32ui.CreateBitmap()
+dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+cDC.SelectObject(dataBitMap)
+cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
+dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
+# Free Resources
+dcObj.DeleteDC()
+cDC.DeleteDC()
+win32gui.ReleaseDC(hwnd, wDC)
+win32gui.DeleteObject(dataBitMap.GetHandle())
